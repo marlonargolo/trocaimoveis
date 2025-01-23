e2c81f6c1c4212c6b89accbb9854d2db233d470b
marlonargolo <argolomarlon@gmail.com>
2025-01-23 01:15:04 -0300

    atualizar templates para chats

--- troca_imoveis/chat/urls.py
+++ troca_imoveis/chat/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.lista_chats, name='lista_chats'),
     path('<int:chat_id>/', views.chat_room, name='chat_room'),
+    path('iniciar/<int:imovel_id>/', views.iniciar_chat, name='iniciar_chat'),
+
 ]
--- troca_imoveis/chat/views.py
+++ troca_imoveis/chat/views.py
@@ -5,3 +5,28 @@ from imoveis.models import Imovel
 def chat_room(request, chat_id):
     chat = get_object_or_404(Chat, id=chat_id)
     return render(request, 'chat_room.html', {'chat': chat})
+
+from django.shortcuts import redirect, get_object_or_404
+from .models import Chat
+from imoveis.models import Imovel
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def iniciar_chat(request, imovel_id):
+    imovel = get_object_or_404(Imovel, id=imovel_id)
+    owner = imovel.proprietario
+
+    # Verificar se o chat já existe
+    chat, created = Chat.objects.get_or_create(
+        imovel=imovel,
+        user=request.user,
+        owner=owner
+    )
+
+    # Redirecionar para o chat
+    return redirect('chat_room', chat_id=chat.id)
+
+@login_required
+def lista_chats(request):
+    chats = Chat.objects.filter(user=request.user)
+    return render(request, 'lista_chats.html', {'chats': chats})
